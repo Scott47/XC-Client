23e32f5ef8f20a97898447c351c24f07d3a60f8b
scottsilver <scottsilver47@gmail.com>
2019-12-10 20:03:28 -0600

    commit before push

--- src/components/team/NewTeam.js
+++ src/components/team/NewTeam.js
@@ -7,6 +7,7 @@ import React, { useRef } from "react";
 
 const NewTeam = props => {
   const team_name = useRef();
+  const coach_id = useRef()
 
 
   // function that adds a team to the teams list on the teams page
@@ -16,8 +17,9 @@ const NewTeam = props => {
     // object that grabs all the values for new team
     const newTeam = {
       team_name: team_name.current.value,
-
+      coach_id: localStorage.getItem("user_id")
     };
+
     createTeam(newTeam).then(() => {
       props.history.push({
         pathname: "/teams"
