3e7ce27d47c651868c4f07463c92752cb0bf3f26
scottsilver <scottsilver47@gmail.com>
2020-01-14 22:06:39 -0600

    added number of runners by team on team list

--- src/components/team/Team.js
+++ src/components/team/Team.js
@@ -1,26 +1,43 @@
 import React from "react";
+import {
+  Container,
+  Card,
+  CardLink,
+  Button,
+  CardFooter,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  CardText,
+  Row,
+  Col
+} from "reactstrap";
 import { Link } from "react-router-dom";
-
+import "./Team.css";
 
 const Team = props => {
   return (
     <>
-      <div
-        className={`team-${props.team.id}`}
-        style={({ height: "2rem" }, { width: "12rem" })}
-      >
-          <div>
-        <section className="team">
-          <Link className="nav-link" to={`/teams/${props.team.id}`}>
-            <p>
-              <strong>
-                {props.team.team_name}
-              </strong>
-            </p>
-          </Link>
-        </section>
-        </div>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Card>
+              <CardBody className="text-center card-body">
+                <CardHeader className="card-team">
+                  <Link className="card-team" to={`/teams/${props.team.id}`}>
+                    <CardLink>
+                      <strong>{props.team.team_name}</strong>
+                    </CardLink>
+                  </Link>
+                </CardHeader>
+                <CardFooter text-size="sm" className="text-muted">
+                  {props.team.runnerteam.length} runners
+                </CardFooter>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
